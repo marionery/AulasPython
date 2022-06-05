7fc053f20bc9804704a830666a7c541c7b4bb67f
marionery <marionery@gmail.com>
2022-06-05 14:20:43 -0300

    problema 6

---
+++ 9 - Numeros ordem decrescente - EstruturaDeDecisao.py
@@ -0,0 +1,13 @@
+#exercicio python 9 - Estrutura de Decisão
+#https://wiki.python.org.br/EstruturaDeDecisao
+
+#Faça um Programa que leia três números e mostre-os em ordem decrescente.
+
+num1 = int(input('Informe o primeiro número: '))
+num2 = int(input('Informe o segundo número: '))
+num3 = int(input('Informe o terceiro número: '))
+
+lista = [num1, num2, num3]
+lista.sort(reverse=True)
+lista_str = str(lista)[1:-1]
+print('Lista em ordem decrescente:', lista_str)
