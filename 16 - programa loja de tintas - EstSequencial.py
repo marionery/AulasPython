0cdeb13147641263759dab4e49bf48d1b5867d7a
marionery <marionery@gmail.com>
2022-06-05 13:36:14 -0300

    problema 16

--- 16 - programa loja de tintas - EstSequencial.py
+++ 16 - programa loja de tintas - EstSequencial.py
@@ -2,8 +2,8 @@
 #https://wiki.python.org.br/EstruturaSequencial
 
 #Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada.
-# Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros,
-# que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
+#Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros,
+#que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
 
 area = float(input('Informe o tamanho da área a ser pintada em M²: '))
 valor_lata = float(input('Informe o valor da lata de 18L:'))
