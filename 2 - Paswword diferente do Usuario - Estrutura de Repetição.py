9c55b65632422735022a83f6e3b60f4d6c5a9dc3
marionery <marionery@gmail.com>
2022-06-08 22:14:23 -0300

    exercício 2

--- 2 - Paswword diferente do Usuario - Estrutura de Repetição.py
+++ 2 - Paswword diferente do Usuario - Estrutura de Repetição.py
@@ -2,7 +2,7 @@
 #https://wiki.python.org.br/EstruturaDeRepeticao
 
 #Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando
-#uma mensagem de erro e voltando a pedir as informações.
+#uma mensagem de erro e voltando a pedir as informações
 
 usuario = ''
 password = ''
